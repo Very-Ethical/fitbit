1690adef7dc62afeb392531de852937a16f0df37
Sam Callahan <callahan.l.sam@gmail.com>
2019-11-20 13:42:56 -0600

    Fixed activity csv production function

--- acquire.py
+++ acquire.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import csv
 
-def make_activity_csv(dir_name):
+def make_activity_csv(dir_name='fitbit'):
     writefile = 'activity.csv'
     write = csv.writer(open(writefile, 'w+'))
     for filename in os.listdir(dir_name):
@@ -12,9 +12,10 @@ def make_activity_csv(dir_name):
             while row != ['Activities']:
                 row = next(reader)
             row = next(reader)
-            if os.stat(writefile).st_size != 0:
-                row = next(reader)
+            if os.stat(writefile).st_size == 0:
+                write.writerow(row)
                 write = csv.writer(open(writefile, 'a'))
+            row = next(reader)
             while row != []:
                 write.writerow(row)
                 row = next(reader)
